a83769217531b0f2f1c5850a926ea8d689d72615
Danil Pismenny <danil@brandymint.ru>
2016-12-20 08:15:59 +0300

    favicons

--- s/js/stats.js
+++ s/js/stats.js
@@ -1,4 +1,4 @@
-var url="https://api.kiiiosk.ru/v1/stats.json";
+var url="//api.kiiiosk.ru/v1/stats.json";
 /* {"companies_count":2205,"selled_items_count":28121,"total_orders_amount":50791571} */
 
 $.getJSON(url, function(data) {
